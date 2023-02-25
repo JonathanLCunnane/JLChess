0d9a44e98f9b66395ac1f01518b7f54f13943651
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-02-25 19:16:36 +0000

    Added a show capture map button to the menu bar.

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,20 @@ public class Main {
         int additionalBorderHeight = 39;
         frame.setBounds(100, 100, 704 + additionalBorderWidth, 704 + additionalBorderHeight);
 
-        JPanel panel = new ChessBoardPanel(board);
+        ChessBoardPanel panel = new ChessBoardPanel(board);
         // Setup frame
         frame.add(panel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setBackground(Color.LIGHT_GRAY);
+
+        // Setup menu bar
+        JMenuBar menuBar = new JMenuBar();
+
+        JButton toggleCaptureMapButton = new JButton("Show Capture Map");
+        toggleCaptureMapButton.addActionListener(new ChessActionListener(panel));
+
+        menuBar.add(toggleCaptureMapButton);
+        frame.setJMenuBar(menuBar);
 
         // Show frame
         frame.setVisible(true);
