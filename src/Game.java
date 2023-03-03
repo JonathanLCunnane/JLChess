80fccec21edf4f92cd190a650c97e28a4ef64a25
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-03-03 18:48:18 +0000

    Implemented vs AI.

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+
+public class Game {
+    Board board;
+    Minimax minimax = new Minimax();
+    boolean versusAI;
+    public Game(boolean isVersusAI, Board gameBoard)
+    {
+        versusAI = isVersusAI;
+        board = gameBoard;
+    }
+
+    public boolean tryMove(Integer[] from, Integer[] to)
+    {
+        if (from[0] == null || from[1] == null || to[0] == null || to[1] == null) return false;
+        if (versusAI && !board.board[from[0]][from[1]].isWhite) return false;
+        return board.tryMove(from, to, true);
+    }
+
+    public void doBlacksBestMove()
+    {
+        if (board.isWhitesMove) return;
+        Map.Entry<Integer[], Integer[]> bestMove = minimax.getBestMove(board, 3);
+        boolean move = board.tryMove(bestMove.getKey(), bestMove.getValue(), true);
+    }
+}
