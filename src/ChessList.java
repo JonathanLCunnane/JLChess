5df990dc632aab34931244ffa805d679fd0283fd
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-03-07 22:36:47 +0000

    Fixed null pointer exception.

--- src/ChessList.java
+++ src/ChessList.java
@@ -9,6 +9,7 @@ public class ChessList {
 
     boolean contains(int[] coordinate)
     {
+        if (list == null) return false;
         for (int[] move: list)
         {
             if (coordinate[0] == move[0] && coordinate[1] == move[1]) return true;
