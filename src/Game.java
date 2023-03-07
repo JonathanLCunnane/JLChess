cc051aed13c65c865b23ccf25834700afa08a9a3
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-03-07 21:49:07 +0000

    Altered the Game class to permit move indicators.

--- src/Game.java
+++ src/Game.java
@@ -12,19 +12,21 @@ public class Game {
     {
         if (from[0] == null || from[1] == null || to[0] == null || to[1] == null) return false;
         if (versusAI && !board.board[from[0]][from[1]].isWhite) return false;
-        return board.tryMove(from, to, true);
+        return board.tryMove(from, to, true, true);
     }
 
-    public void doBlacksBestMove()
+    public Integer[][] doBlacksBestMove()
     {
-        if (board.isWhitesMove) return;
+        if (board.isWhitesMove) return null;
 
         long start = System.nanoTime();
-        Integer[][] bestMove = minimax.getBestMove(board, 6);
+        Integer[][] bestMove = minimax.getBestMove(board, 4);
         long end = System.nanoTime();
         float ms = (end - start)/1000000F;
         System.out.printf("AI's move took %fms\n", ms);
 
-        board.tryMove(bestMove[0], bestMove[1], true);
+        boolean move = board.tryMove(bestMove[0], bestMove[1], true, true);
+        if (move) return bestMove;
+        return null;
     }
 }
